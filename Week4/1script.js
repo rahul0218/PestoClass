3579dca88369dc9224289c04ae1555a53ec6eec5
rahul0218 <kumar.rahul0218@gmail.com>
2022-06-01 17:42:07 +0530

    assignment 2

--- Week4/1script.js
+++ Week4/1script.js
@@ -1,7 +1,30 @@
+// const getNumber=() => new Promise((res, rej)=>{
+//    const randomNumber = parseInt(Math.random() *100);
+//     setTimeout(()=>{
+//         if(randomNumber % 5 === 0){
+//             rej (`rejected  number ${randomNumber}`);
+//         }
+//         res (`Resolve number ${randomNumber}`);
+//     },randomNumber * 10);
 
-   const randomNumber = parseInt(Math.random() *100);
-    if(randomNumber % 5 === 0){
-        PromiseRejectionEvent(`rejected  number ${randomNumber}`);
+// })
+//console.log(randomNumber);
+var Person = function() {};
+
+Person.prototype.initialize = function(name, age)
+{
+    this.name = name;
+    this.age = age;
+}
+
+// TODO: create the class Teacher and a method teach
+var Teacher = function() {
+    this.teach = function(subject){
+        console.log(this.name + " is now teaching " + subject);
     }
-    
+}
+Teacher.prototype = new Person();
+var him = new Teacher();
 
+him.initialize("Adam", 45);
+him.teach("Inheritance");
