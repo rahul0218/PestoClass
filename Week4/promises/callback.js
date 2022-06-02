a7fc81a43ee796d8759192e3064d770a2d447437
rahul0218 <kumar.rahul0218@gmail.com>
2022-06-02 20:34:50 +0530

    callback

---
+++ Week4/promises/callback.js
@@ -0,0 +1,19 @@
+//
+
+// console.log('start');
+
+// setTimeout(()=>{
+//     console.log('middle');
+// }, 0);
+
+// console.log('end');
+
+
+// const fs = require('fs');
+// console.log('start');
+// fs.readFile(__filename,(err, data)=>{
+//     console.log(`data - ${data}`);
+// });
+// console.log('end');
+
+//using call back
