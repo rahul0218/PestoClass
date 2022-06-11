e2c0c3476dae5359901a6e8ace571b8adbe9f850
rahul0218 <kumar.rahul0218@gmail.com>
2022-06-11 14:19:56 +0530

    cycle generators

--- Week5/generator.js
+++ Week5/generator.js
@@ -1,7 +1,6 @@
-//Generators 
+// //Generators 
 // function * gen(){
 //     console.log('Started');
-    
 //     yield 5;
 //     console.log('After 5');
 //     yield 99;
@@ -14,6 +13,15 @@
 // console.log(iter.next());
 // console.log('Do something');
 // console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+
 
 // using loop generators
 // function * gen(){
@@ -68,19 +76,61 @@
 
 
 //cycle
-let i = cycle([1,2,3]);
-function* cycle(iter){
-    let i=0;
-    while(i< iter.lenth){
-        yield iter[i];
-        i++;
-        if (i> iter.lenth){
-            i=0;
-        }
-    }
-}
-
 //let i = cycle([1,2,3]);
 
+// function* cycle(iter){
+//     let i=0;
+//     while(i<= iter.length){
+//         yield iter[i];
+//         i++;
+//         if (i>= iter.length){
+//             i=0;
+//         }
+//     }
+// }
+
+// let i = cycle([1,2,3,4,5,6,7]);
+// console.log(i.next());
+// console.log(i.next());
+// console.log(i.next());
+// console.log(i.next());
+// console.log(i.next());
+// console.log(i.next());
+// console.log(i.next());
+// console.log(i.next());
+// console.log(i.next());
 
 
+//vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+//vowelCount('Colt') // Map { 'o' => 1 }
+
+// function isVowel(char){
+//     return "aeiou".includes(char);
+// }
+// function vowelCount(str){
+//     const vowelMap = new Map();
+//     for(let char of str){
+//         let lowerCaseChar = char.toLowerCase()
+//         if (isVowel(lowerCaseChar)) {
+//             if (vowelMap.has(lowerCaseChar)) {
+//                 vowelMap.set(lowerCaseChar,vowelMap.get(lowerCaseChar)+1);
+//             }else{
+//                 vowelMap.set(lowerCaseChar,1);
+//             }
+//         }
+//     }
+//     return vowelMap;
+// }
+
+
+
+// console.log(vowelCount(''));
+// console.log(vowelCount('Rahul'));
+// console.log(vowelCount('Jitesh'));
+
+
+
+// const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+// console.log(hasDuplicate([1,3,2,5]));
+// console.log(hasDuplicate([1,5,-1,4]));
